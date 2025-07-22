3ee79b5bb58211cc0419f028f8cb98fea547f955
Sabbir45ali <sabbir84b@gmail.com>
2025-07-22 16:25:08 +0530

    Add Tailwind CSS plugin and adjust base path for production mode in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Portfolio/",
-});
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  base: mode === 'production' ? '/Portfolio/' : '/', // Use '/' locally and '/Portfolio/' for GitHub Pages
+}));
